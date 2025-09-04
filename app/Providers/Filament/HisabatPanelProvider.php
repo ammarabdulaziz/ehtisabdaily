ea6cbde6424053e501e820270f33138821af6dd2
Ammar Abdul Aziz <ammarabz10@gmail.com>
2025-09-04 07:07:35 +0300

    Update HisabatPanelProvider.php

--- app/Providers/Filament/HisabatPanelProvider.php
+++ app/Providers/Filament/HisabatPanelProvider.php
@@ -39,7 +39,7 @@ class HisabatPanelProvider extends PanelProvider
             ->path('hisabat')
             ->login()
             ->databaseNotifications()
-            ->brandName('IHITISAB DAILY')
+            ->brandName('EHITISAB DAILY')
             ->colors([
                 'danger' => [
                     50 => 'oklch(0.950 0.040 32.727)',
