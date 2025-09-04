a2ef91e075417ecde1771245185dd8dfa9358688
Ammar Abdul Aziz <ammarabz10@gmail.com>
2025-09-04 11:26:42 +0300

    Update sidebar layout and comment out footer nav items
    Switched AppLayoutTemplate import to use app-sidebar-layout instead of app-header-layout. Commented out the 'Repository' and 'Documentation' footer navigation items in the sidebar component.

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -22,7 +22,7 @@ const mainNavItems: NavItem[] = [
 ];
 
 const footerNavItems: NavItem[] = [
-    {
+    /*{
         title: 'Repository',
         href: 'https://github.com/laravel/react-starter-kit',
         icon: Folder,
@@ -31,7 +31,7 @@ const footerNavItems: NavItem[] = [
         title: 'Documentation',
         href: 'https://laravel.com/docs/starter-kits#react',
         icon: BookOpen,
-    },
+    },*/
 ];
 
 export function AppSidebar() {
