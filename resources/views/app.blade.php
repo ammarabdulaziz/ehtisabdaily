160a025df3918a4939084da640db0116b900397b
Ammar Abdul Aziz <ammarabz10@gmail.com>
2025-09-30 12:33:26 +0300

    Add Amiri font for improved Arabic text rendering
    Introduces the Amiri font to the theme and applies it to Arabic text in the Duas page for better readability. Updates the Blade template to load Amiri from Bunny Fonts and adjusts the DuaCard component's font styling.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,6 +39,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=amiri:400,700" rel="stylesheet" />
 
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
